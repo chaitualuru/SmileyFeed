54e42720fdb32db7a535e24a1f60c2d9b94fae94
kcaluru <krishna_aluru@brown.edu>
2014-03-01 08:12:13 -0500

    Final project

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 README
 ======
 Fill this file in with information on your project, along with any known bugs.
+
+Features:
+- New tweets appear at the top. In fact every appended tweet appears above the last.
+- Maximum tweets at any given point of time is <= 75 depending on if duplicates were present or not. After a point it stays at a constant 75. This is also logged in the console with tweetNumber's value.
+- Removes duplicates. You can check this in console. It logs "skipped duplicates", every time it skips duplicates.
+- Tweet container has overflow:auto, so the tweet container is independently scrollable.
+- Feed refreshes every 5 seconds. 5 seconds is not too long nor too short.
+- When there is no picture associated with the tweet, it displays no_photo image provided in the img folder. This is vary rare though so it might be tough to check for it.
+- Added regex to take care of encoding problem (something is wrong with the utf-8 encoding tag, does not encode all content properly).
+- Added start feed and stop feed features. Start feed also has a slideDown animation. 
+- Converted entities -> user mentions into links.
+
+Additional Features:
+- Made a nice little favicon for the website.
+- Clear clears the feed completely. Press start again to restart querying tweets. Clear also has a slideUp animation.
+- Added some music! (warning: it is a remix, so even if you do not like miley cyrus you can give it a try).
+- Added links to twitter handle, username and profile picture. They have no decoration but you can press on them to check if the links work.
+- When name is "", it displays No Name, empty names are rare but sometimes you will see one or two appear.
+- Used both google fonts and custom fonts.
+- Added border radius to create rounded edges for images.
+
+Note: 
+I use SASS for CSS but at a point I just started typing into CSS so you can ignore the SASS file as well (.scss file).
+
+Issues:
+Old browsers will not be supported especially because the <audio> tag was used in HTML.
--- miley.js
+++ miley.js
@@ -3,32 +3,147 @@
     An implementation of the MileyFeed project.
 */
 
-// This code will be executed when the page finishes loading
-$(document).ready(function(){
-$("#start").click(function(){
-    // Fill me in!
-    // create a request object
-var request = new XMLHttpRequest();
+var tweetNumber = 0;
 
-// specify the HTTP method, URL, and asynchronous flag
-request.open('GET', 'http://miley.djroomba.com/feed/kaluru', true);
+var intId;
 
-// add an event handler
-request.addEventListener('load', function(e){
-    if (request.status == 200) {
-        // do something with the loaded content
-        var content = request.responseText;
-        console.log(content);
-    } else {
-        // something went wrong, check the request status
-        // hint: 403 means Forbidden, maybe you forgot your username?
-    }
-}, false);
+var tweetIds = [];
 
-// start the request, optionally with a request body for POST requests
-request.send(null); 
-}, false);
+$(document).ready(function() {
+	$("#tweet_container").css('visibility', 'hidden');
 
-// Add more supporting code here!
+	$("#start").click(function() {
+		$("#tweet_container").css('visibility', 'visible');
+		$("#tweet_container").hide();
+		$("#tweet_container").slideDown("slow");
+		twtReq('http://miley.djroomba.com/feed/kaluru', twtParse);
+		startReq();
+	});
+
+	$("#stop").click(function() {
+		twtStop();
+	});
+
+	$("#clear").click(function() {
+		$("#tweets").empty();
+		$("#tweet_container").slideUp("slow", function() {
+			$("#tweet_container").show();
+			$("#tweet_container").css('visibility', 'hidden');
+		});
+		twtStop();
+	});
+
+	var cssChanged = false;
+
+	$("#music").click(function() {
+		console.log(cssChanged);
+		if(cssChanged == false) {
+			var music_player = document.createElement('audio');
+			music_player.id = "miley_audio";
+			music_player.src = "audio/wrecking_ball.mp3";
+			music_player.type = "audio/mpeg";
+			document.body.appendChild(music_player);
+			music_player.play();
+			cssChanged = true;
+		}
+		else{
+			$("#miley_audio").remove();
+			cssChanged = false;
+		}
+	});
 });
 
+function startReq() {
+	intId = setInterval(function() {twtReq('http://miley.djroomba.com/feed/kaluru', twtParse);
+									},
+						5000);
+}
+
+function twtReq(theURL, callback) {
+	var request = new XMLHttpRequest();
+
+		request.open('GET', theURL, true);
+
+		request.addEventListener('load', function(e){
+    		if (request.status == 200) {
+        		var content = request.responseText;
+        		callback(content);
+    		} else {
+    			;
+    		}
+		}, false);
+
+		request.send(null); 
+}
+
+function twtParse(jsoncontent) {
+	var data = JSON.parse(jsoncontent);
+    console.log(data);
+    twtDisplay(data);
+}
+
+function twtStop() {
+	clearInterval(intId);
+}
+
+function twtDisplay(tweetcontent) {
+	var ul = document.getElementById('tweets');
+    for (var i=0;i<tweetcontent.length;i++) {
+		var li = document.createElement('li');
+
+		if(tweetcontent[i].user.name === "") {
+				var username = "No Name";
+			}
+			else {
+				var username = tweetcontent[i].user.name;
+			}
+
+		var mentions = tweetcontent[i].entities.user_mentions;
+		var mentions_length = tweetcontent[i].entities.user_mentions.length;
+
+		li.id='tweet';
+
+		var screenname = tweetcontent[i].user.screen_name;
+		var tweet_text = tweetcontent[i].text;
+		if(mentions_length > 0) {
+			for (var k=0; k<1; k++){
+				var mention_string = tweet_text.substring(mentions[k].indices[0], mentions[k].indices[1]);
+				tweet_with_url = tweet_text.replace(mention_string, '<a id = "entity_link" href = https://twitter.com/' + mentions[k].screen_name + '>' + mention_string + '</a>');
+				tweet_text = tweet_with_url;
+			}
+		}
+		else {
+			tweet_with_url = tweet_text;
+		}
+		li.innerHTML = '<a id = "picture_link" href = https://twitter.com/' + screenname + '>' + '<img src ="' + tweetcontent[i].user.profile_image_url + '" id = "userimage" width = "48" height = "48" style = "float:left;" onError="noImage(this)"></img>' + '</a>'
+		+ '<br>' + '<div id="username">' + '<a id = "username_link" href = https://twitter.com/' + screenname + '>' + '<strong>' + username + '</strong>' + '</a>' + '</div>' + '<div id="screenname">' + '<a id = "screenname_link" href = https://twitter.com/' + screenname + '>' 
+		+ '@' + screenname + '</a>' + '</div>'+ '<br>' + '<div id="text">' + tweet_text + '</div>' + '<br>' + '<br>';
+
+		li.innerHTML = li.innerHTML.replace(/\uFFFD/g, '');
+		
+		if(tweetIds.indexOf(tweetcontent[i].id) == -1) {
+            ul.insertBefore(li, ul.childNodes[0]);
+			tweetIds.push(tweetcontent[i].id);
+			tweetNumber++;
+			console.log(tweetNumber);
+		}
+		else {
+			console.log("skipped duplicate/s");
+		}
+
+		if(tweetNumber > 75) {
+			$('#tweets li:last-child').remove();
+			tweetNumber--;
+		}
+		else {
+			;
+		}
+	}
+}
+
+function noImage(image) {
+	console.log('no profile picture existing');
+	image.onerror="";
+	image.src = "img/no_photo.png";
+	return true;
+}
